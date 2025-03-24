d777dadd958acc71019915f61eb06040a8c3ee75
alemalagni <alemalagni@gmail.com>
2025-03-24 16:52:33 +0100

    feat: complete names.js script

--- names.js
+++ names.js
@@ -1,3 +1,10 @@
 function names( firstName, lastName ) {
+    const fullName = {
+        "first name": firstName,
+        "last name": lastName
+    };
 
+    return fullName;
 }
+
+module.exports = fullName;
